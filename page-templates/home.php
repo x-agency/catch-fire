eb880fc75c37bbadd6a0707ef0886e964f88c58d
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-03-02 15:25:34 -0500

    Shawn Commit

--- page-templates/home.php
+++ page-templates/home.php
@@ -8,5 +8,18 @@
     ?>
 <?php get_header(); ?>
 
+<div id="hero">
+    <div class="hero-wrapper">
+        <div class="hero-title"><?php
+				echo file_get_contents( get_theme_file_uri( '/img/jason.svg' ) );
+				?></div>
+        <div class="hero-text">Speaking, Writing, <br>Coaching, and ConsuLting<br> for modern-day leaders</div>
+        <div class="hero-scroll">
+          <div class="scroll-arrow"><img src="/wp-content/themes/catch-fire/img/scroll-arrow.png" alt=""></div>
+          <div class="scroll-text">scroll</div>
+        </div>
+    </div>
+</div>
+
 <?php get_template_part('template-parts/cta') ?>
 <?php get_footer(); ?>
