c93c6809192195af923fcdb0d24b75adcd6e178f
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-03-02 10:44:20 -0500

    First Commit

---
+++ page-templates/home.php
@@ -0,0 +1,12 @@
+<?php
+   /*
+    Template Name: Home
+    */
+    if ( ! defined( 'ABSPATH' ) ) {
+      exit; // Exit if accessed directly.
+    }
+    ?>
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/cta') ?>
+<?php get_footer(); ?>
