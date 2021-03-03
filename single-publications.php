bcd96bea3e8e17b1af583a7f3a55d0b86d6729c1
Calvin-Xagency <Calvin@Xagency.io>
2021-03-03 16:49:01 -0500

    create singles

---
+++ single-publications.php
@@ -0,0 +1,17 @@
+<?php
+    /*
+    Template Name: Books and Publications
+    */
+    if ( ! defined( 'ABSPATH' ) ) {
+        exit; // Exit if accessed directly.
+    }
+?>
+<?php get_header(); ?>
+<?php get_template_part('template-parts/books-pub-hero'); ?>
+
+<section>
+   
+</section>
+
+<?php get_template_part('template-parts/cta'); ?>
+<?php get_footer(); ?>
--- template-parts/books-pub-hero.php
+++ template-parts/books-pub-hero.php
@@ -1,4 +1,4 @@
-<section id="hero">
+<section id="books-pub-hero">
     <img src="/wp-content/themes/catch-fire/img/books-hero.jpg" alt="" class="bg">
     <h1><?php single_post_title(); ?></h1>
     <?php if ( has_post_thumbnail() ) : ?>
