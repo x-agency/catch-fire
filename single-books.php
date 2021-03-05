763f53644b15785f0428c2b99f55d37198a23d56
Calvin-Xagency <Calvin@Xagency.io>
2021-03-05 12:12:15 -0500

    Update Books and Pubs

--- single-books.php
+++ single-books.php
@@ -10,7 +10,19 @@
 <?php get_template_part('template-parts/books-pub-hero'); ?>
 
 <section class="container">
-   
+    <div class="row">
+        <div class="col-lg-6 content">
+            <h2 class="subtitle">OVERVIEW</h2>
+            <h2><?php echo strip_tags( get_the_excerpt() ); ?></h2>
+            <p><?php echo strip_tags( get_the_content() ); ?></p>
+        </div>
+        <div class="col-lg-6 buy-now">
+            <h2 class="subtitle">BUY NOW:</h2>
+            <a href="" class=""><?php echo file_get_contents(__DIR__ . '/img/baker-book-house.svg'); ?></a>
+            <a href="" class=""><img src="/wp-content/themes/catch-fire/img/amazon.png" alt=""></a>
+            <a href="" class="btn">GET FREE CHAPTER</a>
+        </div>
+    </div>
 </section>
 
 <?php get_template_part('template-parts/cta'); ?>
--- single-publications.php
+++ single-publications.php
@@ -9,8 +9,49 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/books-pub-hero'); ?>
 
-<section>
-   
+<section class="container">
+   <div class="row">
+        <div class="col-12">
+            <h2 class="subtitle">OVERVIEW</h2>
+        </div>
+        <div class="col-12">
+            <h2><?php echo strip_tags( get_the_excerpt() ); ?></h2>
+        </div>
+        <?php
+            //prepare the_content() to break into separate paragraphs
+
+            $text_array = explode("\n", get_the_content() );
+            $length = count($text_array);
+
+            $text_first_half = array_slice($text_array, 0, $length / 2);
+            $text_last_half = array_slice($text_array, $length / 2);
+
+        ?>
+        <div class="col-lg-6 pe-lg-5">
+            <?php 
+                // echo the content but not any empty spaces
+                foreach($text_first_half as $text) {
+                    if ( strlen($text) <= 1 ) {
+                        continue;
+                    } else {
+                        echo '<p>' . $text . '</p>';
+                    }
+                }
+            ?>
+        </div>
+        <div class="col-lg-6 ps-lg-5">
+        <?php 
+                // echo the content but not any empty spaces
+                foreach($text_last_half as $text) {
+                    if ( strlen($text) <= 1 ) {
+                        continue;
+                    } else {
+                        echo '<p>' . $text . '</p>';
+                    }
+                }
+            ?>
+        </div>
+   </div>
 </section>
 
 <?php get_template_part('template-parts/cta'); ?>
