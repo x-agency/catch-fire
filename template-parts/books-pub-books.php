0e0eed00a6110c22241d6be7bc4f05f46bb19c2f
Calvin-Xagency <Calvin@Xagency.io>
2021-03-10 13:35:55 -0500

    Resources page JS and Styles

--- template-parts/books-pub-books.php
+++ template-parts/books-pub-books.php
@@ -2,16 +2,24 @@
     <div class="row">
         <div class="col-12"><h2 class="subtitle">BOOKS & PUBLICATIONS</h2></div>
         <div class="col-lg-6 col-xl-3 book">
-            <img src="/wp-content/themes/catch-fire/img/book-come-back.png" alt="">
+            <a href="/books/volunteer-effect/" target="_blank">
+                <img src="/wp-content/themes/catch-fire/img/book-come-back.png" alt="">
+            </a>
         </div>
         <div class="col-lg-6 col-xl-3 book">
-            <img src="/wp-content/themes/catch-fire/img/book-volunteer-survival.png" alt="">
+            <a href="/books/volunteer-effect/" target="_blank">
+                <img src="/wp-content/themes/catch-fire/img/book-volunteer-survival.png" alt="">
+            </a>
         </div>
         <div class="col-lg-6 col-xl-3 book">
-            <img src="/wp-content/themes/catch-fire/img/book-volunteer-effect.png" alt="">
+            <a href="/books/volunteer-effect/" target="_blank">
+                <img src="/wp-content/themes/catch-fire/img/book-volunteer-effect.png" alt="">
+            </a>
         </div>
         <div class="col-lg-6 col-xl-3 book">
-            <img src="/wp-content/themes/catch-fire/img/book-table-influence.png" alt="">
+            <a href="/books/volunteer-effect/" target="_blank">
+                <img src="/wp-content/themes/catch-fire/img/book-table-influence.png" alt="">
+            </a>
         </div>
     </div>
 </section>
