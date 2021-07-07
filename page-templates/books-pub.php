870e6831eb3d08fca7cd6ee238310ce8b3b80463
Calvin-Xagency <Calvin@Xagency.io>
2021-07-07 10:18:08 -0400

    flash paper updates

--- page-templates/books-pub.php
+++ page-templates/books-pub.php
@@ -135,66 +135,6 @@
     <div class="row justify-content-start">
         <div class="col-12"><a href="/flash-papers" target="_blank"><h2 class="subtitle">FLASH PAPERS</h2></a></div>
         <div class="col-12"><h2>We are all busy and at the same time, we want to be in the know about ideas and insights.</h2></div>
-        <?php /*
-        $i = 0;  
-        while(have_rows('flash')) : the_row();
-            $url = get_sub_field('url');
-            $title = get_sub_field('title');
-            $number = get_sub_field('number');
-
-            ?>
-
-            <?php if ($i == 0) : ?>
-                <div class="col-lg-6 col-xl-4">
-            <?php endif; ?>
-                    <a href="<?php echo $url; ?>" class="mb-4">
-                        <div class="flash-paper">
-                            <?php echo file_get_contents(__DIR__ . '/../img/document.svg'); ?>
-                            <p><?php echo get_row_index(); ?></p>
-                        </div>
-                        <p><?php echo $title; ?></p>
-                    </a>
-            <?php $i++; ?>
-            <?php if ($i == 4 ) : ?>
-                </div>
-                <?php $i = 0; ?>
-            <?php endif; ?>
-        <?php endwhile; */?>
-        
-        <?php /*div class="col-lg-6 col-xl-4">
-        <?php for($i = 55; $i >= 52; $i--) : ?>
-            <a href="" class="mb-4">
-                <div class="flash-paper">
-                    <?php echo file_get_contents(__DIR__ . '/../img/document.svg'); ?>
-                    <p><?php echo $i; ?></p>
-                </div>
-                <p>Lorem ipsum dolor sit amet.</p>
-            </a>
-        <?php endfor; ?>
-        </div>
-        <div class="col-lg-6 col-xl-4">
-        <?php for($i = 51; $i >= 48; $i--) : ?>
-            <a href="" class="mb-4">
-                <div class="flash-paper">
-                    <?php echo file_get_contents(__DIR__ . '/../img/document.svg'); ?>
-                    <p><?php echo $i; ?></p>
-                </div>
-                <p>Lorem ipsum dolor sit amet.</p>
-            </a>
-        <?php endfor; ?>
-        </div>
-        <div class="col-lg-6 col-xl-4">
-        <?php for($i = 47; $i >= 44; $i--) : ?>
-            <a href="" class="mb-4">
-                <div class="flash-paper">
-                    <?php echo file_get_contents(__DIR__ . '/../img/document.svg'); ?>
-                    <p><?php echo $i; ?></p>
-                </div>
-                <p>Lorem ipsum dolor sit amet.</p>
-            </a>
-        <?php endfor; ?>
-        </div */?>
-
         <?php
             $posts = get_posts( array(
                 'numberposts' => 9,
@@ -205,7 +145,9 @@
         <?php foreach($posts as $post) : ?>
             <div class="col-lg-6 col-xl-4">
                 <a href="<?php echo get_permalink($pub->ID); ?>" class="mb-4 justify-content-center">
+                    <?php echo file_get_contents( get_theme_file_uri( '/img/flash.svg' ) ); ?>
                     <p><?php echo get_the_title($post->ID); ?></p>
+                    <?php echo file_get_contents( get_theme_file_uri( '/img/arrow.svg' ) ); ?>
                 </a>
             </div>
             <?php $i--; ?>
